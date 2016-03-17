d69beb9caa243c2903d933212a2a643c66983cea
kd2anu <shawn.ko@scanbuy.com>
2016-03-17 19:24:10 +0000

    change cookbook naming to codedeploy-agent

--- codedeploy-agent/metadata.rb
+++ codedeploy-agent/metadata.rb
@@ -1,2 +1,2 @@
-name 'myrecipe'
-recipe 'myrecipe::agent-install', 'Fetches, installs, and starts the AWS CodeDeploy host agent'
+name 'codedeploy-agent'
+recipe 'cocedeploy-agent::agent-install', 'Fetches, installs, and starts the AWS CodeDeploy host agent'
