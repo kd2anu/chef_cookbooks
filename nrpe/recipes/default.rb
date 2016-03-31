7f993c1cc9fa5fe9f541ffb9391e9f732c97ab03
kd2anu <shawn.ko@scanbuy.com>
2016-03-31 17:24:16 +0000

    add nrpe cookbook

---
+++ nrpe/recipes/default.rb
@@ -0,0 +1,35 @@
+user node[:nrpe][:user] do
+  action :create
+  system true
+  shell "/bin/false"
+end
+
+directory node[:nrpe][:dir] do
+  owner "icinga"
+  mode "0755"
+  action :create
+end
+
+remote_directory "/usr/local/nagios" do
+  source "nrpe_files"
+  owner "icinga"
+  group "icinga"
+  mode "0755"
+  action :create_if_missing
+end
+
+template "/etc/init.d/nrpe" do
+  source "nrpe.erb"
+  mode "0755"
+  owner "root"
+  group "root"
+end
+
+bash "add_service" do
+  code "sudo chkconfig --add nrpe"
+  not_if "chkconfig --list |grep -i nrpe"
+end
+
+service "nrpe" do
+  action [:enable, :start]
+end
