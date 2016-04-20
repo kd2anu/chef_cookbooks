694170bef661903fede0db601183922169649cf0
kd2anu <shawn.ko@scanbuy.com>
2016-04-20 04:50:03 +0000

    small fix2

--- jaxws/recipes/default.rb
+++ jaxws/recipes/default.rb
@@ -11,7 +11,7 @@ execute 'unzip_jaxws' do
   group 'root'
   cwd '/opt'
   action :run
-  command 'unzip /opt/jaxws-ri-2.2.10.zip /opt'
+  command 'unzip /opt/jaxws-ri-2.2.10.zip -d /opt'
 #  not_if do ::File.exists?('/opt/jaxws-ri-2.2.10.zip') end
 end
 
