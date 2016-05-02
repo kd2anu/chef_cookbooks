329a20df1995c9caed2fcd1159eb37dc96ea7711
kd2anu <shawn.ko@scanbuy.com>
2016-05-02 20:24:38 +0000

    create log files manually

--- quartz/recipes/quartz_log.rb
+++ quartz/recipes/quartz_log.rb
@@ -1,6 +1,27 @@
-directory '/var/log/coupons' do
+directory '/var/lo /coupons' do
   owner 'tomcat'
   group 'tomcat'
   mode '0766'
+  action :create
+end
+
+file '/var/log/coupons/api2POS.log' do
+  owner 'tomcat'
+  group 'tomcat'
+  mode '0644'
+  action :create
+end
+
+file '/var/log/coupons/quartz.log' do
+  owner 'tomcat'
+  group 'tomcat'
+  mode '0644'
+  action :create
+end
+
+file '/var/log/coupons/trazasCupones.log' do
+  owner 'tomcat'
+  group 'tomcat'
+  mode '0644'
   action :create
 end
